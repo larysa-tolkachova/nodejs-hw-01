12a348572d16f6a0fe57390519aecca62e1c098f
Larysa <seagull2610@gmail.com>
2025-05-04 12:16:05 +0200

    all tasks

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -3,9 +3,14 @@ import { writeContacts } from '../utils/writeContacts.js';
 import { createFakeContact } from '../utils/createFakeContact.js';
 
 const generateContacts = async (number) => {
+  console.log(number);
+
   const oldContacts = await readContacts();
   const newContacts = [];
-  for (let i = 0; i <= number; i++) {
+  for (let i = 1; i <= number; i += 1) {
+    // console.log(number);
+    // console.log(i);
+
     const contact = createFakeContact();
     newContacts.push(contact);
   }
